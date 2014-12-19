4bdd776b8583f1929538fd361a449f6dd306ef6e
Ladislav Škufca <mclado@mclado.home>
2014-12-19 19:29:26 +0100

    Urejen izpis.html, začetek app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var baseUrl = 'https://rest.ehrscape.com/rest/v1';
+var queryUrl = baseUrl + '/query';
+
+var username = "ois.seminar";
+var password = "ois4fri";
+
+function getSessionId() {
+    var response = $.ajax({
+        type: "POST",
+        url: baseUrl + "/session?username=" + encodeURIComponent(username) +
+        "&password=" + encodeURIComponent(password),
+        async: false
+    });
+    return response.responseJSON.sessionId;
+}
