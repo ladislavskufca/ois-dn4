9ce1d3f2cb41fd7b067ce53dc03227532730de3b
Ladislav Škufca <mclado@mclado.home>
2014-12-19 21:33:23 +0100

    Urejanje app.js, manjši popravki v vseh .html datotekah

--- app.js
+++ app.js
@@ -13,3 +13,38 @@ function getSessionId() {
     });
     return response.responseJSON.sessionId;
 }
+
+function preberiEHRzapis() {
+    sessionId = getSessionId();
+
+    var ehrId = $("#preberiEHRid").val();
+
+    if (!ehrId || ehrId.trim().length == 0) {
+        $("#preberiSporocilo").html("<span class='obvestilo label label-warning fade-in'>Prosim vnesite zahtevan podatek!");
+    } else {
+        $.ajax({
+            url: baseUrl + "/demographics/ehr/" + ehrId + "/party",
+            type: 'GET',
+            headers: {"Ehr-Session": sessionId},
+            success: function (data) {
+                var party = data.party;
+
+                $("#preberiSporocilo").html("<span class='obvestilo label label-success fade-in'>Uporabnik '" + party.firstNames + " " + party.lastNames + "', ki se je rodil '" + party.dateOfBirth + "'.</span>");
+                console.log("Uporabnik '" + party.firstNames + " " + party.lastNames + "', ki se je rodil '" + party.dateOfBirth + "'.");
+            },
+            error: function(err) {
+                $("#preberiSporocilo").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
+                console.log(JSON.parse(err.responseText).userMessage);
+            }
+        });
+    }
+}
+
+
+$(document).ready(function() {
+    $('#preberiObstojeciEHR').change(function() {
+        //vsakič ko zamenjamo osebo iz dropdown menija se zgodi naslednje
+        $("#preberiSporocilo").html("");
+        $("#preberiEHRid").val($(this).val());
+    });
+});
